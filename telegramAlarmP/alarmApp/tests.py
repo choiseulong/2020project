8522dc92b147d76180a2b66053e6e3fd5acfed74
seulong <kjkj3468@naver.com>
2020-08-03 17:54:20 +0900

    알구몬 새글 알림 텔레그램 봇 테스트

---
+++ telegramAlarmP/alarmApp/tests.py
@@ -0,0 +1,34 @@
+import telegram, requests, os
+from bs4 import BeautifulSoup
+from apscheduler.schedulers.blocking import BlockingScheduler
+import datetime
+now = datetime.datetime.now()   
+bot = telegram.Bot(token='1369696078:AAGqd5jOU0wLIrF57dUGEXaTh6wiXs33YHE')
+bot.sendMessage(chat_id=1299718052, text="테스트")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+def telegram_bot():
+    url = 'https://algumon.com/'
+    req = requests.get(url)
+    html = req.text
+    soup = BeautifulSoup(html, 'html.parser')
+
+    name = soup.select('div.post-group > div.product-body.clearfix > div > p:nth-child(2) > span > a')
+    latest = name[0].text.strip()
+    latest_link = "https://algumon.com"+str(name[0].attrs['href'])
+    
+    with open(os.path.join(BASE_DIR, 'latest.txt'), 'r+', encoding="utf-8") as f_read :
+        before = f_read.readline()
+        if before != latest :
+            bot.sendMessage(chat_id=1299718052, text="새롭게 추가된 항목\n"+latest+"\n"+latest_link)
+        else :
+            bot.sendMessage(chat_id=1299718052, text="새롭게 추가된 항목이 없습니다\n"+str(now.strftime('%H:%M:%S')))
+        f_read.close()
+    with open(os.path.join(BASE_DIR, 'latest.txt'), 'r+', encoding="utf-8") as f_write :
+        f_write.write(latest)
+        f_write.close()
+
+sched = BlockingScheduler()
+telegram_bot()
+sched.add_job(telegram_bot, 'interval', seconds=10)
+sched.start()
