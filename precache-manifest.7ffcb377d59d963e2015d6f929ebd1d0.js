8b052f0d89819e84ad18e4e715644deb4baa62c2
seulong <kjkj3468@naver.com>
2020-09-03 19:18:17 +0900

    Updates

---
+++ precache-manifest.7ffcb377d59d963e2015d6f929ebd1d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df2d7cf266d0bd7a2dc9913cd11f4f4e",
+    "url": "/2020project/index.html"
+  },
+  {
+    "revision": "84dd83c53edab2e0a32f",
+    "url": "/2020project/static/css/main.ae4cfe19.chunk.css"
+  },
+  {
+    "revision": "11748d6dc8da0fe94e79",
+    "url": "/2020project/static/js/2.8f3b569d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/2020project/static/js/2.8f3b569d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84dd83c53edab2e0a32f",
+    "url": "/2020project/static/js/main.7e0e1364.chunk.js"
+  },
+  {
+    "revision": "32f1bc24a4de64110b28",
+    "url": "/2020project/static/js/runtime-main.617bf961.js"
+  }
+]);
